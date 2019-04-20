bc60ce5b564caaa62f579c257093f617c8b87231
root <root@v120839.hosted-by-vdsina.ru>
2019-04-20 05:32:58 +0300

    Updates

---
+++ precache-manifest.31115b1d952841a394d36c107a2c8f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "04c4ed9fcfacdf095b09",
+    "url": "/vk-apps-currency/static/js/runtime~main.20ce18d6.js"
+  },
+  {
+    "revision": "b1466266502a89218eb6",
+    "url": "/vk-apps-currency/static/js/main.758a614c.chunk.js"
+  },
+  {
+    "revision": "cd33c86af6c7be978b08",
+    "url": "/vk-apps-currency/static/js/2.6cdc6392.chunk.js"
+  },
+  {
+    "revision": "b1466266502a89218eb6",
+    "url": "/vk-apps-currency/static/css/main.9c95e7b7.chunk.css"
+  },
+  {
+    "revision": "cd33c86af6c7be978b08",
+    "url": "/vk-apps-currency/static/css/2.25e7af14.chunk.css"
+  },
+  {
+    "revision": "e4152336c74aa013468b036b974c4855",
+    "url": "/vk-apps-currency/index.html"
+  }
+];
